aade2ab5f84b28a4c0c246166b434e94361d3e1c
Richard Duffy <rduffy@slack-corp.com>
2021-11-16 21:18:28 +0000

    Week 8 Lab

---
+++ scripts/script.js
@@ -0,0 +1,79 @@
+function rock(){
+    var uChoice = 0;
+    hideSelector();
+    showChoice(uChoice)
+    getResult(uChoice)
+}
+
+function paper(){
+    var uChoice = 1;
+    hideSelector();
+    showChoice(uChoice)
+    getResult(uChoice)
+}
+
+function scissors(){
+    var uChoice = 2;
+    hideSelector();
+    showChoice(uChoice)
+    getResult(uChoice)
+}
+
+function hideSelector(){
+    document.getElementById("choiceSurface").style.display = "none";
+}
+
+function showChoice(uChoice){
+    self.uChoice=uChoice;
+    document.getElementById("invite").innerHTML = "You chose:";
+    imagePicker(uChoice,0);
+    document.getElementById("chosen").style.display = "block";
+}
+
+function imagePicker(contest,player){
+    if(player==0){
+        var pic="chosenPic";
+        var tag="chosenTag";
+    } else if (player==1){
+        var pic="pcPic";
+        var tag="pcTag";
+    };
+    if(contest==0){
+        document.getElementById(pic).src="images/rock.png";
+        document.getElementById(pic).alt="Rock";
+        document.getElementById(tag).innerHTML="Rock";
+    }else if (contest==1){
+        document.getElementById(pic).src="images/paper.png";
+        document.getElementById(pic).alt="Paper";
+        document.getElementById(tag).innerHTML="Paper";
+    }else if (contest==2){
+        document.getElementById(pic).src="images/scissors.png";
+        document.getElementById(pic).alt="Scissors";
+        document.getElementById(tag).innerHTML="Scissors";
+    };
+}
+
+function getResult(uChoice){
+    document.getElementById("game").style.display = "block";
+    var result = parseInt(Math.random()*3);
+    setTimeout(function(){
+        imagePicker(result,1);
+        showResult(uChoice,result);
+    },2000);
+}
+
+function showResult(uChoice,result){
+    self.uChoice= uChoice;
+    self.result=result;
+    if (uChoice==result){
+        document.getElementById("result").innerHTML="Draw!";
+    }else if ((uChoice==0 && result==1) || (uChoice==1 && result==2) || (uChoice==2 && result==0)){
+        document.getElementById("result").innerHTML="You lose!";
+    }else if ((uChoice==0 && result==2) || (uChoice==1 && result==0) || (uChoice==2 && result==1)){
+        document.getElementById("result").innerHTML="You win!";
+    }
+    document.getElementById("shuffler").innerHTML="The PC chose:";
+    setTimeout(function(){
+        document.getElementById("refresh").style.display = "block";
+    },1500);
+}
